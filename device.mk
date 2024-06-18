5f86373e7620f1751540abbab9e259433db26dc9
Vaisakh Murali <vaisakhmurali@gmail.com>
2024-06-18 22:48:00 +0530

    sky: Switch to OSS libperfmgr
    Entirely re-written powerhint.
    
    Change-Id: Ib4dfdb7fafd0e0bab6591fe516a3d87e42a06ffe

--- device.mk
+++ device.mk
@@ -382,10 +382,12 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.lineage-libperfmgr \
+    libperfmgr.vendor \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/parrot/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
@@ -486,6 +488,10 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
+    hardware/bliss/interfaces/power-libperfmgr \
+    hardware/google/interfaces \
+    hardware/google/pixel/power-libperfmgr/libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
 # Telephony
